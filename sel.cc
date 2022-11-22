41dda6a6afc6adcaf0eb2ee027e24321fbb8395c
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-11-22 08:39:20 +0100

    Update sel.cc

--- sel.cc
+++ sel.cc
@@ -1,25 +1,14 @@
--- antin019: Samarbetat med omiaz320, Omid Azimi, samma program
+// antin019: Samarbetat med omiaz320, Omid Azimi, samma program
 #include <iostream> 
 #include <string> 
 #include <iomanip>
 #include <cctype>
 
   using namespace std;
-int main(){
-
-    int start;
-    int slut;
-    char cha;
-    string str;
-    string short;
-    string long;
-    int letters = 0;
-    int num = 0;
-    int blank = 0;
 
 ///cel_kel/////////////
 
-    cel_kel(int const item){
+    double cel_kel(int const item){
         
         double temp;
 
@@ -29,7 +18,7 @@ int main(){
     
 ///cel_far////////////
 
-    cel_far(int const item){
+    double cel_far(int const item){
 
         double temp;
 
@@ -39,7 +28,7 @@ int main(){
 
 ///cel_rem/////////////
 
-    cel_rem(int const item){
+    double cel_rem(int const item){
 
         double temp;
 
@@ -49,61 +38,100 @@ int main(){
 
 ///average////////////
 
-    average(int const start, int const slut){
-      double temp;
-      temp = (start+slut)/2;
-      return temp;
+    double average(string long_word, string short_word){
+      
+      return (long_word.size() + short_word.size()) / 2;;
     }
+//Main///////////////
+  
+int main(){
 
+///Variable//////////
+
+    int start;
+    int end;
+    int letters = 0;
+    int num = 0;
+    int blank = 0;
+    char cha;
+    bool no_text_buffer = true;
+    string str;
+    string short_word;
+    string long_word;
+    
 
 ///////////////////////
-    cout<<"Del 1: Temperaturtabell"<<endl;    
-    while(start > slut){
-    <<"Ange startvärde: ";
-    cin>>start;
-    cout<<"Ange slutvärde: "
-    cin>>slut;
-    if(start < slut){
-        cout<<"Felaktigt slutvärde!"<<endl;
-    }
-    }
 
-    cout<<"Celsius Kelvin Fahrenheit Reaumur"<<endl;
-    cout<<"---------------------------------------"<<endl;
-        for(int i; start = slut; start++){
-            cout<<start<<cel_kel(start)<<cel_far(start)<<cel_rem(start)<<endl;
+    cout<<"Del 1: Temperaturtabell"<<endl;    
+    do{
+    cout<<"Ange startvärde: ";
+    cin>>start;
+    cout<<"Ange slutvärde: ";
+    cin>>end;
+    if(start > end){
+        cout<<"Felaktigt slutvärde!"<<endl;
+        cin.ignore(1000,'\n');
+    }
+    }while(start > end);
+    cout<<endl<<endl;
+
+
+    cout<<"Celsius Kelvin Fahrenheit Reaumur"<<endl//Skriver ut fel antal decimaler
+    <<"---------------------------------------"<<endl;
+        while(start<=end){
+            cout<<setw(7)<<start<<setw(7)<<cel_kel(start)
+            <<setw(11)<<cel_far(start)<<setw(8)<<cel_rem(start)<<endl;
+            start++;
         }
+    cout<<"---------------------------------------"<<endl<<endl;
+    cin.ignore(1000,'\n');
 
   cout<<"Del 2: Teckenhantering"<<endl;
-    for(int i; i = 0; i < 11; i++){
+    for(int i{}; i < 10; i++){
     cin.get(cha);
       if(isdigit(cha)){
-        num = num +1;
+        num++;
       }
-      else if(isalpha(cha)){
-        letters = letters + 1;
+      if(isalpha(cha)){
+        letters++;
       }
-      else if(isspace(cha)){
-        blank = blank + 1;
+      if(isspace(cha)){
+        blank++;
       }
+    }
 
-    cin.ignore(10,'\n');
-
-  cout<<"Texten innehöll:"<<endl<<"Alfabetiska tecken:"<<letters<<endl;
-  cout<<"Siffertecken......:"<<num<<endl<<"Vita tecken.......:"<<blank<<endl;
+  cout<<"Texten innehöll:"<<endl<<"Alfabetiska tecken:"<<letters<<endl
+  <<"Siffertecken......:"<<num<<endl<<"Vita tecken.......:"<<blank<<endl<<endl;
 
 
   cout<<"Del 3: Ordhantering"<<endl<<"Mata in en text:"<<endl;
-  cin.ignore(1000,'\n');
-  getline(cin,str);
+  num = 0;
 
+    while(cin>>str){
 
+        no_text_buffer = false;
 
-  cout<<"Texten innehöll "<<num<<" ord."<<endl;
-  cout<<"Det kortaste ordet var "<<short<<" med "<<start<<" tecken."<<endl;
-  cout<<"Det längsta ordet var "<<long<<" med "<<slut<<" tecken."<<endl;
-  cout<<"Medelordlängden var "<<average(start,slut)<<" tecken."<<endl;
-
-
-
+        if(num == 0){
+          short_word = str;
+          long_word = str;
+        }
+        if(str.size() > long_word.size()){
+          long_word = str;
+        }
+        if(str.size() < short_word.size()){
+          short_word = str;
+        }
+        num++;
     }
+
+    if(no_text_buffer){
+      cout<<"Inga ord matades in."<<endl;
+    }
+  
+    else{
+    cout<<"Texten innehöll "<<num<<" ord."<<endl;
+    cout<<"Det kortaste ordet var "<<short_word<<" med "<<short_word.size()<<" tecken."<<endl;
+    cout<<"Det längsta ordet var "<<long_word<<" med "<<long_word.size()<<" tecken."<<endl;
+    cout<<"Medelordlängden var "<<average(long_word,short_word)<<" tecken."<<endl;
+    }
+ }
