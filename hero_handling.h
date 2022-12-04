0ed89fd5d3a7f6d53f77eb5f17b7f43ec0cb6b36
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-04 23:03:15 +0100

    Update hero_handling.h

--- hero_handling.h
+++ hero_handling.h
@@ -1,9 +1,11 @@
+// antin019: Samarbetat med omiaz320, Omid Azimi, samma program
 //header_guard
 # ifndef HERO_HANDLING_H
 # define HERO_HANDLING_H
 
 # include <string>
 # include <vector>
+# include <fstream>
 
 //private
 
@@ -17,9 +19,16 @@
     };
 using register_type = std::vector<hero_type>;
 
-    void add(register_type & heroes);
-    void search(register_type & heroes,
-                register_type & matches);
+    bool operator ==(hero_type const a,
+                     hero_type const b);
+    bool compare_matches(hero_type const item,
+                         int const compare);
+    void print_to_stream(hero_type const item,
+                         std::ostream & write_to);
+    void print_hero(hero_type const item);
+    void feed_struct(hero_type & hero);
+    bool sort_by_name(hero_type const a,
+                      hero_type const b);
 
 # endif //header guard /HERO_HANDLING.H
 
