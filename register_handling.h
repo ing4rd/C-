33a61a4bb4fcb6591122d4a813090272a4aa9591
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-04 23:05:03 +0100

    Update register_handling.h

--- register_handling.h
+++ register_handling.h
@@ -1,14 +1,19 @@
+// antin019: Samarbetat med omiaz320, Omid Azimi, samma program
 //header_guard
 # ifndef REGISTER_HANDLING_H
 # define REGISTER_HANDLING_H
 
-#include  "hero_handling.h"
 #include <vector>
+#include <string>
 
+#include  "hero_handling.h"
 
 using register_type = std::vector<hero_type>;
 
-    void get_register_1(register_type & item);
-    void add_to_register_1(register_type & item);
+    void search(register_type & heroes,
+                register_type & matches,
+                std::string const file_name);
+    void add(register_type & heroes,
+             std::string const file_name);
 
 # endif
