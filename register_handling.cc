12f24e8167b0d97ce924d0db8b3ac409e727656a
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-05 21:25:03 +0100

    Update register_handling.cc

--- register_handling.cc
+++ register_handling.cc
@@ -29,8 +29,8 @@ using namespace std;
 
 ////////
 
-    void feed_struct_from_stream(register_type & item,
-                                 istream & read_from){
+    void feed_struct_from_stream(register_type  & item,
+                                 istream        & read_from){
 
         hero_type hero;
         string text;
@@ -52,15 +52,11 @@ using namespace std;
 
 /////////
 
-    void add_to_register(register_type & item,
-                         string const & file_name){
+    void add_to_register(register_type       & item,
+                         string        const & file_name){
 
         ofstream write_to;
         string text;
-
-        if(!(write_to.is_open())){
-            //cout<<"Did not open file in add_to_register!";
-        }
 
         write_to.open(file_name);
             for(int i = 0; i < item.size(); i++){
@@ -71,14 +67,10 @@ using namespace std;
 
 /////////
 
-    void get_register(register_type & item,
-                        string const & file_name){
+    void get_register(register_type       & item,
+                        string      const & file_name){
         
         ifstream read_from;
-
-        if(!(read_from.is_open())){
-            //cout<<"Did not open file in get_register!";
-        }
 
         read_from.open(file_name);
             while(!read_from.eof()){
@@ -89,10 +81,9 @@ using namespace std;
 
 ////////
 
-    void add(register_type & heroes,
-             string const & file_name){
+    void add(register_type       & heroes,
+             string        const & file_name){
 
-        hero_type hero;
         bool new_hero_added = false;
 
         get_register(heroes,file_name);
@@ -101,7 +92,8 @@ using namespace std;
 
                 cout<<"Enter hero information:"<<endl;
 
-                    feed_struct(hero);
+                hero_type hero{};
+                feed_struct(hero);
 
                     if(check_for_existing_hero(heroes,hero)){
                         cout<<"Hero already in register. ";
@@ -129,10 +121,10 @@ using namespace std;
 
 ////////
 
-    void search_register(register_type & heroes,
-                         register_type & matches,
-                         int const intrest,
-                         string const & file_name){
+    void search_register(register_type       & heroes,
+                         register_type       & matches,
+                         int           const   intrest,
+                         string        const & file_name){
 
         get_register(heroes,file_name);
 
@@ -149,47 +141,46 @@ using namespace std;
 
     void feed_like(vector<int> & item){
 
-        stringstream ss;
         string text;
         int num;
-        bool data_has_not_been_read = true;
-        bool still_more_data = false;
+        bool data_has_been_read = false;
 
-        do{ 
-            cin >> num;
-            if(!(num < 1) && !(num > 15)){
-                item.push_back(num);
-                data_has_not_been_read = false;
-                    if(cin.peek() != '\n'){
-                        still_more_data = true;
-                    }
-                }
-        }while(still_more_data && data_has_not_been_read);
+            do{
+                getline(cin, text);
+                stringstream ss{text};
+
+                while(ss >> num){
+                    if(num >= 1 && num <= 15){
+                        item.push_back(num);
+                        data_has_been_read = true;
+                            }
+                        }
+            }while(!data_has_been_read);
 
         sort(item.begin(),item.end());
-        cin.ignore(1000,'\n');
     }
 
 ////////
 
-    void search(register_type & heroes,
-                register_type & matches,
-                string  const & file_name){
+    void search(register_type      & heroes,
+                register_type      & matches,
+                string       const & file_name){
 
         vector<int> like;
 
         cout<<"Enter your interests (at least one between 1 and 15): ";
         feed_like(like);
 
-        for(int i{}; i < like.size(); i++){
-            search_register( heroes,
-                             matches,
-                             like.at(i),
-                             file_name);
-                             }
+            for(int i{}; i < like.size(); i++){
+                search_register( heroes,
+                                 matches,
+                                 like.at(i),
+                                 file_name);
+                                 }
         cout<<"There are "<<matches.size()<<" matching heroes."<<endl;
         cout<<"Hero name  Birth year  Weight  Hair color  Interests"<<endl;
         cout<<"===================================================="<<endl;
+        sort(matches.begin(),matches.end(),sort_by_name);
         print_matches(matches);
         matches.clear();
     }
