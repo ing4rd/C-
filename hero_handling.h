fda3bb583ae16e5dda541a9083b0280feade9f9e
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-05 14:30:05 +0100

    Update hero_handling.h

--- hero_handling.h
+++ hero_handling.h
@@ -19,16 +19,16 @@
     };
 using register_type = std::vector<hero_type>;
 
-    bool operator ==(hero_type const a,
-                     hero_type const b);
-    bool compare_matches(hero_type const item,
+    bool operator ==(hero_type const & a,
+                     hero_type const & b);
+    bool compare_matches(hero_type const & item,
                          int const compare);
-    void print_to_stream(hero_type const item,
+    void print_to_stream(hero_type const & item,
                          std::ostream & write_to);
-    void print_hero(hero_type const item);
+    void print_hero(hero_type const & item);
     void feed_struct(hero_type & hero);
-    bool sort_by_name(hero_type const a,
-                      hero_type const b);
+    bool sort_by_name(hero_type const & a,
+                      hero_type const & b);
 
 # endif //header guard /HERO_HANDLING.H
 
