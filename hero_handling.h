6fe076c585b7790b218804ded9e0697b76c67f39
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-07 15:54:35 +0100

    Update hero_handling.h

--- hero_handling.h
+++ hero_handling.h
@@ -9,26 +9,25 @@
 
 //private
 
-    struct hero_type
-    {
-        std:: string name;
-        std:: string bithyear;
-        std:: string weight;
-        std:: string hair_color;
-        std:: vector<int> intrest;
-    };
-using register_type = std::vector<hero_type>;
+struct Hero_type
+{
+  std:: string name;
+  std:: string bithyear;
+  std:: string weight;
+  std:: string hair_color;
+  std:: vector<int> intrest;
+};
 
-    bool operator ==(hero_type const & a,
-                     hero_type const & b);
-    bool compare_matches(hero_type const & item,
-                         int const compare);
-    void print_to_stream(hero_type const & item,
-                         std::ostream & write_to);
-    void print_hero(hero_type const & item);
-    void feed_struct(hero_type & hero);
-    bool sort_by_name(hero_type const & a,
-                      hero_type const & b);
+bool operator ==(Hero_type const & a,
+		 Hero_type const & b);
+bool compare_matches(Hero_type const & item,
+		     int const compare);
+void print_to_stream(Hero_type const & item,
+		     std::ostream & write_to);
+void print_hero(Hero_type const & item);
+void feed_struct(Hero_type & hero);
+bool sort_by_name(Hero_type const & a,
+		  Hero_type const & b);
 
 # endif //header guard /HERO_HANDLING.H
 
