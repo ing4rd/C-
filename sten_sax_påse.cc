f18f0cb2e4c9283820d5b28915873f396c89e79a
ing4rd <108624661+ing4rd@users.noreply.github.com>
2022-12-17 14:21:17 +0100

    Update sten_sax_påse.cc

--- sten_sax_påse.cc
+++ sten_sax_påse.cc
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+//============================
+
+string print_string(int const value){
+
+    if(value == 1){
+        return "STEN";
+    }
+    else if(value == 2){
+        return "SAX";
+    }
+    else if(value == 3){
+        return "PÅSE";
+    }
+    else{
+        return "ANNAT";
+    }
+}
+
 //============================
 
 bool deciding_winner(int &AI,
                      int &player,
                      bool &winner)
 {
-    if (AI == player)
+    if (AI == player|| player == 4)
     {
+        cout << "Datorn valde: " << print_string(AI) << endl << "Du valde: "<< print_string(player) << '\n' <<endl;
         return true;
     }
     else if ((AI == 1 && player != 3) || (AI == 2 && player != 1) || (AI == 3 && player != 2))
@@ -28,46 +51,39 @@ void seting_values(int &AI,
                    int &player)
 
 {
-    int rnd_num{rand()};
+    bool bad_value{false};
+    int const rnd_num{rand()};
     string temp;
 
-    do
-    {
-        bool bad_value{false};
-        switch (rnd_num)
-        {
-        case rnd_num <= (RAND_MAX / 3):
+
+        if(rnd_num < (RAND_MAX / 3)){
             AI = 1;
-            break;
-
-        case rnd_num >= ((RAND_MAX / 3) * 2):
+        }
+        else if(rnd_num > ((RAND_MAX / 3)*2)){
             AI = 2;
-
-        default:
+        }
+        else{
             AI = 3;
-            break;
         }
 
         cin >> temp;
-        if (temp == 'STEN' || temp == "sten")
+        if (temp == "STEN" || temp == "sten")
         {
             player = 1;
         }
         else if (temp == "sax")
         {
-            player == 2;
+            player = 2;
         }
         else if (temp == "påse")
         {
-            player == 3;
+            player = 3;
         }
         else
         {
-            bad_value = true;
-            cin.ignore(1000, '\n');
+            player = 4;
         }
 
-    } while (bad_value)
 }
 
 //============================
@@ -78,26 +94,28 @@ void play()
     int AI;
     int player;
     bool winner;
+    vector<string> ssp{"STEN","SAX","PÅSE"};
 
     do
     {
         for (int i{}; i < 3; i++)
         {
-            wait();
-            cout << i;
+            sleep(2);
+            cout << ssp.at(i)<<'\n' <<endl;
         }
         seting_values(AI, player);
+        cin.ignore(1000, '\n');
 
     } while (deciding_winner(AI, player, winner));
 
-    cout << AI << "      " << player << ;
+    cout << "Datorn valde: " << print_string(AI) << endl << "Du valde: "<< print_string(player) << '\n' <<endl;
     if (winner)
     {
-        cout << "Du vann, gratulerar!"
+        cout << "Du vann, gratulerar!"<< endl;
     }
     else
     {
-        cout << "Du förlora, attans!";
+        cout << "Du förlora, attans!"<< endl;
     }
 }
 //=============================
@@ -115,11 +133,11 @@ int main()
             play();
         }
 
-        else
+        else if(cha != 'e')
         {
             cout << "Du matande in ett felaktigt värde!" << endl;
         }
-    } while (cha == 'e');
+    } while (cha != 'e');
 
     cout << "Tack för att du spelade!";
 
